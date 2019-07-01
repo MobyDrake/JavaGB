8a3263a85c008f5510508a024222147740b388af
MobyDrake <milkky598@yandex.ru>
2019-07-01 20:57:58 +0500

    lesson7

--- src/main/java/lesson7/Solution.java
+++ src/main/java/lesson7/Solution.java
@@ -1,4 +1,57 @@
 package lesson7;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Comparator;
+import java.util.LinkedList;
+
 public class Solution {
+
+
+    public static void start(Class testClass) {
+        Method[] methods = testClass.getDeclaredMethods();
+        LinkedList<Method> list = new LinkedList<>();
+
+        for(Method m : methods) {
+            if(m.isAnnotationPresent(Test.class)) {
+                int priority = m.getAnnotation(Test.class).priority();
+                if (priority < 1 || priority > 10) {
+                    throw new RuntimeException();
+                }
+                list.add(m);
+            }
+        }
+
+        list.sort(new Comparator<Method>() {
+            @Override
+            public int compare(Method o1, Method o2) {
+                return o2.getAnnotation(Test.class).priority() - o1.getAnnotation(Test.class).priority();
+            }
+        });
+
+        for(Method m : methods) {
+            if (m.isAnnotationPresent(BeforeSuite.class)) {
+                if (list.get(0).isAnnotationPresent(BeforeSuite.class)) {
+                    throw new RuntimeException();
+                }
+                list.set(0, m);
+            }
+
+            if (m.isAnnotationPresent(AfterSuite.class)) {
+                if (list.get(list.size()-1).isAnnotationPresent(AfterSuite.class)) {
+                    throw new RuntimeException();
+                }
+                list.add(m);
+            }
+        }
+
+        for(Method m : list) {
+            try {
+                m.invoke(testClass);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
--- src/main/java/lesson7/Test.java
+++ src/main/java/lesson7/Test.java
@@ -8,6 +8,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface SolTest {
+public @interface Test {
     int priority() default 5;
 }
